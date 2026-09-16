bb1fab291bed0737f23ecd4f820f95f96141754f
noklash <njokugoodluck2k@gmail.com>
2026-09-16 06:55:44 +0100

    added command line arguments

--- 03-fswatch/src/main.c
+++ 03-fswatch/src/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
     struct stat st;
 
-    if (stat("README.md", &st) == -1) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s info <path>\n", argv[0]);
+        return 1;
+    }
+
+    if (stat(argv[2], &st) == -1) {
         perror("stat");
         return 1;
     }
