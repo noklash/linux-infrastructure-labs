b995435ec031c75e1ecdd3df96493b9c4123f212
noklash <njokugoodluck2k@gmail.com>
2026-09-16 07:38:55 +0100

    added changelog

---
+++ 03-fswatch/CHANGELOG.md
@@ -0,0 +1,26 @@
+# Changelog
+
+All meaningful project changes are recorded here.
+
+## Unreleased
+
+### Added
+
+- Initial project structure.
+- Initial filesystem inspection command.
+- Reproducible Makefile build.
+- Initial automated test framework.
+- Initial filesystem engineering documentation.
+
+### Planned
+
+- Complete filesystem object classification.
+- Symbolic link inspection.
+- Inode and filesystem identity inspection.
+- Hard-link experiments.
+- Ownership and permission inspection.
+- Timestamp inspection.
+- Directory traversal.
+- Recursive filesystem inspection.
+- Mount and filesystem boundary analysis.
+- Storage allocation analysis.
--- 03-fswatch/src/main.c
+++ 03-fswatch/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <inttypes.h>
 
 int main(int argc, char *argv[])
 {
@@ -15,14 +16,24 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    printf("Size: %ld bytes\n", st.st_size);
+    printf("Size: %" PRIdMAX " bytes\n", (intmax_t)st.st_size);
 
     if (S_ISREG(st.st_mode)) {
         printf("Type: regular file\n");
     } else if (S_ISDIR(st.st_mode)) {
         printf("Type: directory\n");
+    } else if (S_ISLNK(st.st_mode)) {
+        printf("Type: symbolic link\n");
+    } else if (S_ISCHR(st.st_mode)) {
+        printf("Type: character device\n");
+    } else if (S_ISBLK(st.st_mode)) {
+        printf("Type: block device\n");
+    } else if (S_ISFIFO(st.st_mode)) {
+        printf("Type: FIFO\n");
+    } else if (S_ISSOCK(st.st_mode)) {
+        printf("Type: socket\n");
     } else {
-        printf("Type: other\n");
+        printf("Type: unknown\n");
     }
 
     return 0;
